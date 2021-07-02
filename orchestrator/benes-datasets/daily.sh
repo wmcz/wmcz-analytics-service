990c51b565c16805759030ce1823fc2696dcd55d
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-07-02 18:49:12 +0200

    benes-datasets: Also load to Toolforge DB

--- orchestrator/benes-datasets/daily.sh
+++ orchestrator/benes-datasets/daily.sh
@@ -6,6 +6,7 @@ echo /tmp/$$
 for dataset in website other; do
 	# download dataset from WMCZ server
 	wget -O /tmp/$$/$dataset.tar.gz https://files.wikimedia.cz/datasets/$dataset.tar.gz
+	wget -O /tmp/$$/$dataset.sql.gz https://files.wikimedia.cz/datasets/$dataset.sql.gz
 
 	cd /tmp/$$
 	tar xzf $dataset.tar.gz
@@ -15,6 +16,9 @@ for dataset in website other; do
 	mkdir -p ~/public_html/datasets/periodic/$dataset
 	mv $dataset ~/public_html/datasets/periodic/$dataset/daily
 	mv $dataset.tar.gz ~/public_html/datasets/periodic/$dataset/daily.tar.gz
+
+	# load data to Toolforge's database
+	zcat /tmp/$$/$dataset.sql.gz | mysql s53887__wmcz_${dataset}_p
 done
 
 rm -rf /tmp/$$
